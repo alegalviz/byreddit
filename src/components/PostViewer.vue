0e49fb0469921dd62f72f2d7cef8b23bd6a2582c
Alejandro Galviz <alegalviz@gmail.com>
2019-10-08 17:32:59 -0300

    Responsive fix

--- src/components/PostViewer.vue
+++ src/components/PostViewer.vue
@@ -1,24 +1,30 @@
 <template>
-  <div class="post-viewer">
-    <v-card
-      class="mx-auto"
-      :flat="flat"
-      :loading="loading"
-      :height="height"
-      v-if="post"
-    >
-      <img
-        v-if="post.thumbnail"
-        class="white--text"
-        height="200px"
-        :src="`${post.thumbnail}?id=${post.id}`"
-      >
-      <v-card-title>{{ post.title }}</v-card-title>
+  <v-container fluid fill-height>
+    <v-layout row wrap align-center>
+      <v-flex xs12>
+        <div class="post-viewer">
+          <v-card
+            class="mx-auto"
+            :flat="flat"
+            :loading="loading"
+            :height="height"
+            v-if="post"
+          >
+            <img
+              v-if="post.thumbnail"
+              class="white--text"
+              height="200px"
+              :src="`${post.thumbnail}?id=${post.id}`"
+            >
+            <v-card-title>{{ post.title }}</v-card-title>
 
-      <v-card-text>Link: <a :href="`${baseImagURL}${post.permalink}`" target="_blank">{{ post.title }}</a></v-card-text>
-      <v-card-text>Author: {{ post.author }}</v-card-text>
-    </v-card>
-  </div>
+            <v-card-text>Link: <a :href="`${baseImagURL}${post.permalink}`" target="_blank">{{ post.title }}</a></v-card-text>
+            <v-card-text>Author: {{ post.author }}</v-card-text>
+          </v-card>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 <script>
 export default {
